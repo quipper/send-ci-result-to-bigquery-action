0d45c61a9a210278f4b40cdd95ff30ab0b8f4911
Hidetake Iwata <int128@gmail.com>
2023-08-05 14:41:24 +0900

    Support `<testsuites>`

--- src/junit.ts
+++ src/junit.ts
@@ -14,43 +14,88 @@ export const parseXML = (s: string): TestResult => {
   return xml
 }
 
-export type TestResult = {
-  testsuite: {
-    testcase: {
-      '@_classname': string
-      '@_name': string
-      '@_file': string
-      '@_time': number
-      failure?: {
-        '#text': string
-      }
-    }[]
+export const flattenTestCases = (xml: TestResult): TestCase[] => {
+  if (xml.testsuites) {
+    return [xml.testsuites.testsuite].flat().flatMap((testSuite) => testSuite.testcase)
   }
+  if (xml.testsuite) {
+    return [xml.testsuite.testcase].flat()
+  }
+  return []
+}
+
+export type TestResult = {
+  testsuite?: TestSuite
+  testsuites?: TestSuites
 }
 
 function assertTestResult(x: unknown): asserts x is TestResult {
   assert(typeof x === 'object')
   assert(x != null)
 
-  assert('testsuite' in x)
-  assert(typeof x.testsuite === 'object')
-  assert(x.testsuite != null)
-
-  assert('testcase' in x.testsuite)
-  assert(Array.isArray(x.testsuite.testcase))
-  for (const testcase of x.testsuite.testcase) {
-    assertTestcase(testcase)
+  if ('testsuite' in x) {
+    assertTestSuite(x.testsuite)
+  }
+  if ('testsuites' in x) {
+    assertTestSuites(x.testsuites)
   }
 }
 
-function assertTestcase(x: unknown): asserts x is TestResult['testsuite']['testcase'] {
+type TestSuites = {
+  testsuite: TestSuite | TestSuite[]
+}
+
+function assertTestSuites(x: unknown): asserts x is TestSuites {
+  assert(typeof x === 'object')
+  assert(x != null)
+  assert('testsuite' in x)
+  if (Array.isArray(x.testsuite)) {
+    for (const testsuite of x.testsuite) {
+      assertTestSuite(testsuite)
+    }
+    return
+  }
+  assertTestSuite(x.testsuite)
+}
+
+type TestSuite = {
+  testcase: TestCase | TestCase[]
+}
+
+function assertTestSuite(x: unknown): asserts x is TestSuite {
+  assert(typeof x === 'object')
+  assert(x != null)
+  assert('testcase' in x)
+  if (Array.isArray(x.testcase)) {
+    for (const testcase of x.testcase) {
+      assertTestCase(testcase)
+    }
+    return
+  }
+  assertTestCase(x.testcase)
+}
+
+type TestCase = {
+  '@_classname': string
+  '@_name': string
+  '@_time': number
+  '@_file'?: string
+  failure?: {
+    '#text': string
+  }
+}
+
+export function assertTestCase(x: unknown): asserts x is TestCase {
   assert(typeof x === 'object')
   assert(x != null)
   assert('@_classname' in x)
   assert(typeof x['@_classname'] === 'string')
   assert('@_name' in x)
-  assert('@_file' in x)
   assert('@_time' in x)
+
+  if ('@_file' in x) {
+    assert(typeof x['@_file'] === 'string')
+  }
 
   if ('failure' in x) {
     assert(typeof x.failure === 'object')
